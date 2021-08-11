83f088702066f68cfb27bf134149c9077944390b
MacBook Pro <Nazaf@MacBook-Pro.local>
2021-08-11 06:39:41 +0600

    calculations seem to be working fine

--- main.js
+++ main.js
@@ -2,64 +2,59 @@ const bill = document.getElementById("bill");
 const reset = document.getElementById("reset");
 const inputFields = document.getElementsByClassName("inputField");
 const people = document.getElementById("people");
-const tip = document.getElementById("tip-output");
+const tip = document.getElementsByClassName("button");
+const custom = document.getElementById("custom");
+const tipOutput = document.getElementById("tip-output");
 const total = document.getElementById("total-output");
 
 reset.addEventListener('click', resetAll);
 bill.addEventListener('keyup', keypressDetect);
 people.addEventListener('keyup', keypressDetect);
+custom.addEventListener('keyup', customTip);
 
-
+var tipPercent = 0;
 
 
 window.onload = resetAll;
 
+for (i = 0; i < tip.length; i++){
+    tip[i].addEventListener("click", tipDetect);
+}
+
+// WHEN A CUSTOM TIP AMOUNT IS ENTERED
+function customTip(e) {
+    tipPercent = e.target.value;
+    billCalculate();
+}
+
+
+// WHEN A TIP BUTTON IS SELECTED
+function tipDetect(e){
+    custom.value = "";
+    tipPercent = parseFloat(e.target.value);
+    billCalculate();
+    
+}
 
 
 
-console.log(people.value);
-
+// CLEARS OUT ALL INPUT FIELD ON PAGE REFRESH
 function resetAll(e){
     for (i = 0; i < inputFields.length; i++){
         inputFields[i].value = "";
     };
+    custom.value = "";
     people.value = 1;
 }
 
 
+// WHENEVER KEYPRESS OCCURS IN ANY INPUT FIELD
 function keypressDetect(e){
-    if(people.value == 0 || people.value == ""){
-        document.getElementById("people-error").className = "people-error-show";
-        total.innerText = `N/A`;
-    } else {
-        document.getElementById("people-error").className = "people-error";
-
-        var billValue = bill.value;
-
-        if(billValue == ""){
-            total.innerText = `$0.00`;
-        } else {
-            grossBill = parseFloat(billValue);
-            totalPP = grossBill/people.value;
-            total.innerText = parseFloat(totalPP).toFixed(2);     
-        }
-    }
-
-
-
-
-    // var billValue = bill.value;
-
-    // if(billValue == ""){
-    //     total.innerText = `$0.00`;
-    // } else {
-    //     grossBill = parseFloat(billValue);
-    //     totalPP = grossBill/people.value;
-    //     total.innerText = parseFloat(totalPP).toFixed(2);     
-    // }
+    billCalculate();
 }
 
 
+// ALLOWS ONLY NUMERIC VALUES IN THE BILL FIELD
 function isNumberKey(txt, evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode;
     if (charCode == 46) {
@@ -78,6 +73,41 @@ function isNumberKey(txt, evt) {
   }
 
 
-  function setTwoNumberDecimal(event) {
+// RESTRICTS ENTERED BILL TO 2 DECIMAL PLACES IN OUTPUT
+function setTwoNumberDecimal(event) {
     return parseFloat(event.target.value).toFixed(2);
-}
+};
+
+
+// BILL CALCULATION
+function billCalculate(){
+    if(people.value == 0 || people.value == ""){
+        document.getElementById("people-error").className = "people-error-show";
+        total.innerText = `N/A`;
+        tipOutput.innerText = `N/A`;
+    } else if (bill.value == 0 && people.value > 0) {
+        document.getElementById("people-error").className = "people-error";
+        var tipAmount = 0;
+        var tipPP = 0;
+        tipOutput.innerText = parseFloat(tipPP).toFixed(2);
+        total.innerText = parseFloat(0).toFixed(2);
+    } 
+    
+    
+    else {
+        document.getElementById("people-error").className = "people-error";
+
+        // var billValue = bill.value;
+
+        if(bill.value == ""){
+            total.innerText = `$0.00`;
+        } else {
+            var grossBill = parseFloat(bill.value);
+            var tipAmount = grossBill * (tipPercent/100);
+            var tipPP = tipAmount / people.value;
+            tipOutput.innerText = parseFloat(tipPP).toFixed(2);
+            totalPP = (grossBill + tipAmount) / people.value;
+            total.innerText = parseFloat(totalPP).toFixed(2);     
+        }
+    }
+};
